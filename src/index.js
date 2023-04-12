391b0c51a9d520bc24cf9ba40b5180fa201d828e
Kennedy Muinde <kennedymuinde@gmail.com>
2023-04-12 10:25:54 +0300

    Displayed all toys

--- src/index.js
+++ src/index.js
@@ -12,4 +12,26 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  //Function to show toys
+  function showToys() {
+    const toysDiv = document.querySelector("#toy-collection");
+    fetch("http://localhost:3000/toys")
+      .then((res) => res.json())
+      .then((toys) => {
+        toys.forEach((toy) => {
+          const card = document.createElement("div");
+          card.classList.add("card");
+          card.innerHTML = `
+            <h2>${toy.name}</h2>
+            <img src="${toy.image}" class="toy-avatar"/>
+            <p>Likes: ${toy.likes}</p>
+            <button class="button">LIKE</button>
+          `;
+          toysDiv.appendChild(card);
+          console.log(card);
+        });
+      });
+  }
+  showToys();
 });
